be20e79215333955dd0b39def61885e06e98e5b1
Viktoria Likai <Likay-V@yandex.ru>
2022-10-01 11:28:15 +0900

    Решение 4 модуля

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  
+	for(let propertyName of friends){
+    let li = document.createElement('li');
+    console.log(propertyName.firstName);
+    li.innerHTML = `${propertyName.firstName} ${propertyName.lastName}`;
+    ul.append(li);		
+  }
+  return ul;
 }
