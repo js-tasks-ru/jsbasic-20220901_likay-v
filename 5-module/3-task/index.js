d41f8e5f3d2df7f48de3d83893bd8ece2989c627
Viktoria Likai <Likay-V@yandex.ru>
2022-10-08 14:04:29 +0900

    Решение 5 модуля

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,42 @@
 function initCarousel() {
-  // ваш код...
+  let right = document.querySelector('.carousel__arrow_right');
+  let left = document.querySelector('.carousel__arrow_left');
+  let carousel = document.querySelector('.carousel__inner');
+  let index = 0;
+
+  if(index == 0){
+    left.style.display = 'none';
+  }
+  
+  right.addEventListener('click',() => {
+    index++;
+    let step = carousel.offsetWidth * -index;
+    carousel.style.transform = 'translateX(' + step + 'px)';
+    checkV(index, right, left, carousel);
+  })
+
+  left.addEventListener('click',() => {
+    index--;
+    let step = carousel.offsetWidth * -index;
+    carousel.style.transform = 'translateX(' + step + 'px)';
+    checkV(index, right, left, carousel);
+  })
+
+}
+
+function checkV(index, right, left, carousel){
+  
+    if(index == carousel.childElementCount - 1){
+      right.style.display = 'none';
+    } else {
+      right.style.display = '';
+    }  
+  
+    if(index == 0){
+      left.style.display = 'none';
+    } else {
+      left.style.display = '';
+    }
+  
+
 }
